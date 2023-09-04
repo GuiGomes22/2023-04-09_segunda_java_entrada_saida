2cab9735c610660f8bcf7e73edb4f1ce429c794b
Guilherme Gomes <gui-h-gomes@hotmail.com>
2023-09-04 19:48:55 -0300

    ap02(input-output): captura dois valores

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+//pacote padrão: java.lang: importado automaticamento
+public class SomaDoisNumeros{
+    public static void main(String [] args){
+        //declaração de variáveis
+        Scanner leitor; = new Scanner(System.in);
+        double primeiroOperando;
+        double segundoOperando;
+        double resultado;
+
+        //entrada
+        System.out.println("Digite o primeiro operando");
+        primeiroOperando = leiton.nextDouble();
+        system.out.println("Digite o segundo operando")
+        segundo.out = leiton.nextDouble();
+        //processamento
+
+        //saída
+    }
+}
